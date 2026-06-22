a4e8faf3d54e4805ff29a74937f4ef1cb4356aaa
Yujun <pathfinder357@github.com>
2026-06-22 13:11:31 +0900

    Controller 이해하기

---
+++ src/main/java/com/sprata/springmvc2/controller/HelloController.java
@@ -0,0 +1,44 @@
+package com.sprata.springmvc2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api")
+public class HelloController {
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "hello";
+	}
+
+	@GetMapping("/get")
+	@ResponseBody
+	public String get() {
+		return "get";
+	}
+
+	@PostMapping("/post")
+	@ResponseBody
+	public String post() {
+		return "post";
+	}
+
+	@PutMapping("/put")
+	@ResponseBody
+	public String put() {
+		return "put";
+	}
+
+	@DeleteMapping("/delete")
+	@ResponseBody
+	public String delete() {
+		return "delete";
+	}
+
+}
