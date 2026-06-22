afb3afef33523fd68bdac21b640725189c1a7e88
Yujun <pathfinder357@github.com>
2026-06-22 14:54:51 +0900

    정적페이지&동적페이지

---
+++ src/main/java/com/sprata/springmvc2/html/HtmlController.java
@@ -0,0 +1,33 @@
+package com.sprata.springmvc2.html;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HtmlController {
+
+	private static long visitcount = 0;
+
+	@GetMapping("/static-hello")
+	public String hello() {
+		return "hello.html";
+	}
+
+	@GetMapping("/html/redirect")
+	public String redirect() {
+		return "redirect:/hello.html";
+	}
+
+	@GetMapping("/html/templates")
+	public String templates() {
+		return "hello";
+	}
+
+	@GetMapping("/html/dynamic")
+	public String dynamic(Model model) {
+		visitcount++;
+		model.addAttribute("visits", visitcount);
+		return "hello-visit";
+	}
+}
