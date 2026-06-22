9fcf753ede72ddc39c9b9508cd36c018eb71a8f8
Yujun <pathfinder357@github.com>
2026-06-22 19:06:05 +0900

    http data convert to obj

--- src/main/java/com/sparta/springmvc2/request/RequestController.java
+++ src/main/java/com/sparta/springmvc2/request/RequestController.java
@@ -2,8 +2,10 @@ package com.sparta.springmvc2.request;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -46,5 +48,36 @@ public class RequestController {
 		return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
 	}
 
+	// [Request sample]
+	// POST http://localhost:8080/hello/request/form/model
+	// Header
+	//  Content type: application/x-www-form-urlencoded
+	// Body
+	//  name=Robbie&age=95
+	@PostMapping("/form/model")
+	@ResponseBody
+	public String helloRequestBodyForm(@ModelAttribute Star star) {
+		return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+	}
+
+	// [Request sample]
+	// GET http://localhost:8080/hello/request/form/param/model?name=Robbie&age=95
+	@GetMapping("/form/param/model")
+	@ResponseBody
+	public String helloRequestParam(@ModelAttribute Star star) {
+		return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+	}
+
+	// [Request sample]
+	// POST http://localhost:8080/hello/request/form/json
+	// Header
+	//  Content type: application/json
+	// Body
+	//  {"name":"Robbie","age":"95"}
+	@PostMapping("/form/json")
+	@ResponseBody
+	public String helloPostRequestJson(@RequestBody Star star) {
+		return String.format("Hello, @RequestBody.<br> (name = %s, age = %d) ", star.name, star.age);
+	}
 
 }
---
+++ src/main/java/com/sparta/springmvc2/request/Star.java
@@ -0,0 +1,14 @@
+package com.sparta.springmvc2.request;
+
+public class Star {
+
+	String name;
+	int age;
+
+	public Star(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+
+}
